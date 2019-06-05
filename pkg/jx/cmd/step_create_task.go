40128339ae9c0561235bacd8ec991b338665fd66
yangrenjiang <yangrenjiang@ztinfo.com>
2019-06-05 10:50:12 +0800

    添加日志

--- pkg/jx/cmd/step_create_task.go
+++ pkg/jx/cmd/step_create_task.go
@@ -1704,6 +1704,8 @@ func (o *StepCreateTaskOptions) modifyStep(projectConfig *config.ProjectConfig, 
 			sourceDir := o.getWorkspaceDir()
 			dockerfile := filepath.Join(sourceDir, "Dockerfile")
 			localRepo := o.getDockerRegistry(projectConfig)
+
+			log.Infof("my log projectConfig:%s", projectConfig)
 			log.Infof("my log localRepo:%s", localRepo)
 			destination := o.dockerImage(projectConfig, gitInfo)
 
