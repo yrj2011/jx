0a73362ea51adfcde4f39d9c6193337d76608698
yangrenjiang <yangrenjiang@ztinfo.com>
2019-06-05 08:57:36 +0800

    修改pack的文件路径

--- pkg/jenkinsfile/gitresolver/buildpack.go
+++ pkg/jenkinsfile/gitresolver/buildpack.go
@@ -18,16 +18,16 @@ func InitBuildPack(gitter gits.Gitter, packURL string, packRef string) (string, 
 		return "", fmt.Errorf("Failed to parse build pack URL: %s: %s", packURL, err)
 	}
 
-	log.Infof("pack git url:%s", u)
-	u.Host = strings.Replace(u.Host, "https://", "http://", 1)
-	u.Host = strings.Replace(u.Host, "http://github.com", "http://root:y5QBwLzXE4HYAUMXuG3A@192.168.1.228:1080", 1)
-	u.Host = strings.Replace(u.Host, "http://api.github.com", "http://root:y5QBwLzXE4HYAUMXuG3A@192.168.1.228:1080", 1)
-	log.Infof("pack git url:%s", u)
 	draftDir, err := util.DraftDir()
 	if err != nil {
 		return "", err
 	}
 	dir := filepath.Join(draftDir, "packs", u.Host, u.Path)
+	log.Infof("pack git dir:%s", u)
+	dir = strings.Replace(dir, "https://", "http://", 1)
+	dir = strings.Replace(dir, "github.com", "192.168.1.228:1080", 1)
+	dir = strings.Replace(dir, "api.github.com", "192.168.1.228:1080", 1)
+	log.Infof("pack git dir:%s", u)
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		return "", fmt.Errorf("Could not create %s: %s", dir, err)
 	}
