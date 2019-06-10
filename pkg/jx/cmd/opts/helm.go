26dd0e6377291609a7a911e85a621188cae751cb
yangrenjiang <yangrenjiang@ztinfo.com>
2019-06-10 19:55:46 +0800

    修改helmIP

--- pkg/jx/cmd/opts/helm.go
+++ pkg/jx/cmd/opts/helm.go
@@ -276,6 +276,7 @@ func (o *CommonOptions) RegisterLocalHelmRepo(repoName, ns string) error {
 	log.Infof("my log chartmuseum:%s", chartmuseum)
 	helmUrl = strings.Replace(helmUrl, "chartmuseum.jx.192.168.1.105.nip.io", chartmuseum, 1)
 	helmUrl = "http://admin:123456@" + chartmuseum
+	log.Infof("my log helmUrl:%s", helmUrl)
 	// lets check if we already have the helm repo installed or if we need to add it or remove + add it
 	remove := false
 	repos, err := o.Helm().ListRepos()
